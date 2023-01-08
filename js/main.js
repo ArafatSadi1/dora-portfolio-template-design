fbc739bea7f104d0fe3e7ba90aa542d1b61359ab
ArafatSadi1 <arafatsadi1@gmail.com>
2023-01-08 22:00:49 +0600

    contact form design

--- js/main.js
+++ js/main.js
@@ -34,4 +34,20 @@ $(document).ready(function () {
       },
     },
   });
+  // test
+  $(".popup-with-zoom-anim").magnificPopup({
+    type: "inline",
+
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: "auto",
+
+    closeBtnInside: true,
+    preloader: false,
+
+    midClick: true,
+    removalDelay: 300,
+    mainClass: "my-mfp-zoom-in",
+  });
 });
