a060f9e125231967d76b6d644fff36be192328ae
ArafatSadi1 <arafatsadi1@gmail.com>
2023-01-08 14:29:45 +0600

    use magnific popup

--- js/main.js
+++ js/main.js
@@ -3,48 +3,27 @@ let index = 0;
 $(document).ready(function () {
   // filter gallery
   $(".works-list").click(function () {
-    const value = $(this).attr("data-filter");
-    if (value == "all") {
-      $(".work-item").show("1000");
-      $(".work-item-cont").css({ "flex-direction": "column" });
-    } else {
-      $(".work-item")
-        .not("." + value)
-        .hide("1000");
-      $(".work-item")
-        .filter("." + value)
-        .show("1000");
-      $(".work-item-cont").css({ "flex-direction": "row" });
-    }
-  });
-  // add active class on filter gallery selected item
-  $(".works-list").click(function () {
     $(this).addClass("active").siblings().removeClass("active");
-  });
 
-  // set lightbox img max height
-  const wHeight = $(window).height();
-  $(".lightbox-img").css("max-height", wHeight + "px");
-  // lightbox
-  $(".work-item-inner").click(function () {
-    index = $(this).parent(".work-item").index();
-    $(".works-lightbox").addClass("open");
-    lightboxOpen();
-  });
-  // close lightbox
-  $(".lightbox-close").click(function () {
-    $(".works-lightbox").removeClass("open");
-  });
+    var filter = $(this).attr("data-filter");
 
-  // close lightbox when clicked outside of img-box
-  $(".works-lightbox").click(function (event) {
-    if ($(event.target).hasClass("works-lightbox")) {
-      $(this).removeClass("open");
+    if (filter == "all") {
+      $(".image").show(400);
+    } else {
+      $(".image")
+        .not("." + filter)
+        .hide(200);
+      $(".image")
+        .filter("." + filter)
+        .show(400);
     }
+  });
+  // magnific popup
+  $(".works-items").magnificPopup({
+    delegate: "a",
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
   });
 });
-
-function lightboxOpen() {
-  const imgSrc = $(".work-item").eq(index).find("img").attr("data-modal");
-  $(".lightbox-img").attr("src", imgSrc);
-}
