16ae26e4f7f8611ad10bfab8c4e830b9555e4c4a
ArafatSadi1 <arafatsadi1@gmail.com>
2023-01-08 16:36:59 +0600

    work section image overlay and popup title

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
 
     if (filter == "all") {
       $(".image").show(400);
+      $(".work-item-cont").css("flex-direction", "column");
     } else {
       $(".image")
         .not("." + filter)
@@ -16,14 +17,21 @@ $(document).ready(function () {
       $(".image")
         .filter("." + filter)
         .show(400);
+      $(".work-item-cont").css("flex-direction", "row");
     }
   });
+
   // magnific popup
   $(".works-items").magnificPopup({
     delegate: "a",
     type: "image",
     gallery: {
       enabled: true,
+    },
+    image: {
+      titleSrc: function (item) {
+        return item.el.attr("title");
+      },
     },
   });
 });
