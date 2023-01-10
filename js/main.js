b3b82dedc5852af9a4bb7aa4e856ef8db983b226
ArafatSadi1 <arafatsadi1@gmail.com>
2023-01-10 22:41:23 +0600

    add breakpoint on slider

--- js/main.js
+++ js/main.js
@@ -111,10 +111,15 @@ var swiper = new Swiper(".services-cont", {
       slidesPerView: 1,
       spaceBetween: 10,
     },
-    // when window width is >= 640px
-    640: {
+    // when window width is >= 766px
+    766: {
       slidesPerView: 3,
       spaceBetween: 20,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 30,
     },
   },
 });
@@ -136,16 +141,14 @@ var swiper = new Swiper(".feedback-items-cont", {
     prevEl: ".feedback-swiper-button-prev",
   },
   breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
     // when window width is >= 640px
-    640: {
+    766: {
       slidesPerView: 1,
       spaceBetween: 30,
     },
+    767: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
   },
-
 });
